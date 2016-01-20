01292c2ab8c635c1ad3fd9fe94c2d0c3e8987708
shankarbala33 <shankarbala33@users.noreply.github.com>
2016-01-20 20:47:17 +0530

    Create SQL_Injection
    Anti-SQL injection functions for make sure on your portal security.

---
+++ class.SQL_injection.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Product     : SQL Injection 
+ * Description : Anti-SQL injection function for make sure on your portal security,
+ * Author      : Shankar Thiyagaraajan,
+ * Mail        : shankarthiyagaraajan@gmail.com.
+ */
